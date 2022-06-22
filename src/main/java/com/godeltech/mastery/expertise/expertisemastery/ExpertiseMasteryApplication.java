c2ffe924a00915809be0efe1f8505dba5d3e0e10
s.mitskevich <s.mitskevich@godeltech.com>
2022-06-22 00:02:58 -0700

    Project initialization

---
+++ src/main/java/com/godeltech/mastery/expertise/expertisemastery/ExpertiseMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.godeltech.mastery.expertise.expertisemastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertiseMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertiseMasteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godeltech/mastery/expertise/expertisemastery/ExpertiseMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godeltech.mastery.expertise.expertisemastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertiseMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
